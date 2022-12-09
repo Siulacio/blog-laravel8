fbd4775577405b018cabf572105ac3b69e3acb6a
Siulacio <siulacio@hotmail.com>
2022-12-09 15:54:06 -0500

    feat: add Project basic setup
    model, controller, seeder, migration and factory

---
+++ database/migrations/2022_12_09_185357_create_projects_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("user_id");
+            $table->foreign("user_id")->references("id")->on("users");
+            $table->string("name", 140)->unique();
+            $table->text("description")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,20 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
+    public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+         User::factory()->times(1)->create([
+             "name" => "cursosdesarrolloweb",
+             "email" => "app@cursosdesarrolloweb.es",
+             "password" => bcrypt("password")
+         ]);
+
+         Project::factory()->times(40)->create();
     }
 }
