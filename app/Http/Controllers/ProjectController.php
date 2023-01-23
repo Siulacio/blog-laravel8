04b4b1b37d4e98633cba559d4cf5ecc2e9e7282e
Siulacio <siulacio@hotmail.com>
2023-01-23 06:41:50 -0500

    feat: move middleware to routes

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -9,11 +9,6 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware("auth");
-    }
-
     public function index(): View
     {
         $projects = Project::with("user")->paginate(10);
--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,6 @@
 use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -21,5 +10,5 @@ Auth::routes();
 
 Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::resource("projects", ProjectController::class);
+Route::resource("projects", ProjectController::class)->middleware(['auth', 'verified']);
 
