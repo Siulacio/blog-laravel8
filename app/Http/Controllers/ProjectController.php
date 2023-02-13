16703a622669f6d5dd39321d66a5e06cf21fabd2
Siulacio <siulacio@hotmail.com>
2023-02-13 08:39:06 -0500

    refactor: viewmodel for create and edit methods

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ProjectRequest;
 use App\Models\Project;
+use App\ViewModels\UpsertProjectViewModel;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 
@@ -17,11 +18,8 @@ class ProjectController extends Controller
 
     public function create(): View
     {
-        $project = new Project();
-        $title = __('Crear proyecto');
-        $textButton = __('Crear');
-        $route = route('projects.store');
-        return view('projects.create', compact('title', 'textButton', 'route', 'project'));
+        $viewModel = new UpsertProjectViewModel();
+        return view('projects.create', $viewModel->toArray()['form_data']);
     }
 
     public function store(ProjectRequest $request): RedirectResponse
@@ -36,11 +34,8 @@ class ProjectController extends Controller
 
     public function edit(Project $project): View
     {
-        $update = true;
-        $title = __('Editar proyecto');
-        $textButton = __('Actualizar');
-        $route = route('projects.update', ['project' => $project]);
-        return view('projects.edit', compact('update','title', 'textButton', 'route', 'project'));
+        $viewModel = new UpsertProjectViewModel($project);
+        return view('projects.edit', $viewModel->toArray()['form_data']);
     }
 
     public function update(ProjectRequest $request, Project $project): RedirectResponse
---
+++ app/ViewModels/UpsertProjectViewModel.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\ViewModels;
+
+use App\Models\Project;
+
+final class UpsertProjectViewModel extends ViewModel
+{
+    public $project;
+    public function __construct(?Project $project = null)
+    {
+        $this->project = $project;
+    }
+
+    public function formData(): array
+    {
+        if ($this->project) {
+            return $this->updateFormData();
+        }
+
+        return $this->createFormData();
+    }
+
+    protected function createFormData(): array
+    {
+        $project = new Project();
+        $title = __('Crear proyecto');
+        $textButton = __('Crear');
+        $route = route('projects.store');
+        return compact('title', 'textButton', 'route', 'project');
+    }
+
+    protected function updateFormData(): array
+    {
+        $project = $this->project;
+        $update = true;
+        $title = __('Editar proyecto');
+        $textButton = __('Actualizar');
+        $route = route('projects.update', ['project' => $this->project]);
+        return compact('project', 'update','title', 'textButton', 'route');
+    }
+
+}
